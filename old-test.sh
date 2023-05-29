76273801e19a0ee1e9611d05ff467dc349266fb6
lptpre <lptpro@liupoinshuyuan.com>
2023-05-29 16:27:28 +0800

    update old

--- old-test.sh
+++ old-test.sh
@@ -1,6 +1,2 @@
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/CouponActivityAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/CouponActivityAction.class.php
-cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HuatiV283Action.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/HuatiV283Action.class.php
-cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/MinicourseAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/MinicourseAction.class.php
-cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/MininotifyAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/MininotifyAction.class.php
-cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/MiniorderAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/MiniorderAction.class.php
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Model/CouponActivityModel.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Model/CouponActivityModel.class.php
