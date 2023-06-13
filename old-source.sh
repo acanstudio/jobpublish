77728dd69728cdcdc880e7b3693c2a2b3db69e2c
predeveloper <predeveloper@liupinshuyuan.com>
2023-06-13 13:31:29 +0800

    update old

--- old-source.sh
+++ old-source.sh
@@ -1,4 +1,5 @@
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/AdminHuatiAction.class.php /home/www/selfpath//old/apps/dakaprogram/Lib/Action/AdminHuatiAction.class.php
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HuatiAlbumAction.class.php /home/www/selfpath//old/apps/dakaprogram/Lib/Action/HuatiAlbumAction.class.php
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/AdminMzAction.class.php /home/www/selfpath//old/apps/dakaprogram/Lib/Action/AdminMzAction.class.php
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HomeindexAction.class.php /home/www/selfpath//old/apps/dakaprogram/Lib/Action/HomeindexAction.class.php
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HuatiV283Action.class.php /home/www/selfpath//old/apps/dakaprogram/Lib/Action/HuatiV283Action.class.php
--- old-test.sh
+++ old-test.sh
@@ -1,3 +1,8 @@
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/AdminHuatiAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/AdminHuatiAction.class.php
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HuatiAlbumAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/HuatiAlbumAction.class.php
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/AdminMzAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/AdminMzAction.class.php
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HomeindexAction.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/HomeindexAction.class.php
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Lib/Action/HuatiV283Action.class.php /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Lib/Action/HuatiV283Action.class.php
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/_static/js/admin_ziliao.js /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/_static/js/admin_ziliao.js
 cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Tpl/default/AdminHuati/saveHuatiCategoryHtml.html /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Tpl/default/AdminHuati/saveHuatiCategoryHtml.html
+cp /usr/local/nginx/html/old.liupinshuyuan.com//apps/dakaprogram/Tpl/default/AdminHuati/setHuatiCategoryHtml.html /usr/local/nginx/html/selfdev_liupin//apps/dakaprogram/Tpl/default/AdminHuati/setHuatiCategoryHtml.html
